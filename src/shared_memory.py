8de3145505d2fca210f8d9c500ef39597969c994
Jens21 <jens-beisswenger@gmx.de>
2022-12-17 09:23:41 +0100

    Updated SharedMemory

--- src/shared_memory.py
+++ src/shared_memory.py
@@ -1,21 +1,21 @@
 import threading
 
 class SharedMemory():
-    policy_net_dict = None
-    target_net_dict = None
+    _policy_net_dict = None
+    _target_net_dict = None
 
     def __init__(self):
         lock = threading.Lock()
 
     def set_nets_dict(self, policy_net_dict, target_net_dict):
         self.lock.acquire()
-        self.policy_net_dict = policy_net_dict
-        self.target_net_dict = target_net_dict
+        self._policy_net_dict = policy_net_dict
+        self._target_net_dict = target_net_dict
         self.lock.release()
 
     def get_nets_dict(self):
         self.lock.acquire()
-        pol, tar = self.policy_net_dict, self.target_net_dict
+        pol, tar = self._policy_net_dict, self._target_net_dict
         self.lock.release()
 
         return pol, tar
